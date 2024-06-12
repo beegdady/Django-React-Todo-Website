bd6ea24de2aa62b343fd5ccf2609fe4904df16e6
beegdady <adelekemanuel@gmail.com>
2024-06-12 07:46:01 -0700

    Added the functionality to make the django app run by creating model, views, serializer and model reg in admin

--- todoBackend/todoBackend/settings.py
+++ todoBackend/todoBackend/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#The CORS allowed Origins
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
 
 # Application definition
 
@@ -37,11 +41,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # The externaml apps installed
+    'rest_framework',
+    'corsheaders',
+
+    # The internal apps
+    'todo'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
