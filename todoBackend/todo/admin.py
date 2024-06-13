b9c21d26c6d76982cf2167ab45dff3adbfe981bc
beegdady <adelekemanuel@gmail.com>
2024-06-13 04:07:06 -0700

    completed the creation of the api

--- todoBackend/todo/admin.py
+++ todoBackend/todo/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . import models
 
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['body', 'completed', 'created', 'updated']
+
 # Register your models here.
-admin.site.register(models.Todo)
+admin.site.register(models.Todo, TodoAdmin)
