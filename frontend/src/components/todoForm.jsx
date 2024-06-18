d758acff87291e70577699c4cd3e063612e3575b
beegdady <adelekemanuel@gmail.com>
2024-06-18 15:17:22 -0700

    added the todo form using the components from shadcn-ui

--- frontend/src/components/todoForm.jsx
+++ frontend/src/components/todoForm.jsx
@@ -1,7 +1,20 @@
+import { Form, FormItem } from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+
+
 const TodoForm = () => {
     return (
         <div>
-            Todo Form
+            <Form>
+                <FormItem>
+                    <div className='flex max-w-full items-center gap-2 mt-6 lg:w-1/2'>
+                        <Input className ='border-black'/>
+                        <Button>Add Todo</Button>
+                    </div>
+                    
+                </FormItem>
+            </Form>
         </div>
     )
 }
